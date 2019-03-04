a83d9fc1f2bbe27ba7e8890ee4998ebf4c585861
KerstingJ <Kersting.Josh@Gmail.com>
2019-03-04 16:17:48 -0500

    updated readme

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// lets do our navigation!
+const nav = document.querySelectorAll("nav a");
+const navItemKeys = Object.keys(siteContent.nav).filter(x => x.includes("nav"));
+
+nav.forEach(function(item, index){
+  item.textContent = siteContent.nav[navItemKeys[index]];
+});
+
+//lets get the cta set uppp
+const ctaItems = Object.keys(siteContent.cta);
+ctaItems.forEach(function(item){
+  if (item.includes("img")){
+    const ctaElement = document.querySelector(`.cta img`);
+    ctaElement.src = siteContent.cta[item];
+  } else {
+    const ctaElement = document.querySelector(`.cta ${item}`);
+    ctaElement.textContent = siteContent.cta[item];
+  }
+})
+
+
+//main-content now. lets break it down one piece at a time
+const h4s = document.querySelectorAll(".main-content h4");
+const paras = document.querySelectorAll(".main-content p");
+//i'm sure there is a way to put these together into one object of heading/content pairs
+// but idk this works and that might overcomplicate it
+const h4Keys = Object.keys(siteContent["main-content"]).filter(x => x.includes("h4"));
+const paraKeys = Object.keys(siteContent["main-content"]).filter(x => x.includes("content"));
+
+// im lowkey proud of this
+h4s.forEach(function(h4Element, index){
+  //we're gonna take advantage that h4 and para are the same length
+  // and have corresponding indices and do both in one go
+  h4Element.textContent = siteContent["main-content"][h4Keys[index]];
+  paras[index].textContent = siteContent["main-content"][paraKeys[index]];
+});
+
+//always gotta be an oddball
+const mainImg = document.querySelector(".main-content img");
+mainImg.src = siteContent["main-content"]["middle-img-src"];
+
+//lets get a contact in hur
+const contacts = document.querySelector(".contact h4");
+contacts.textContent = siteContent.contact["contact-h4"];
+
+const conP = document.querySelectorAll(".contact p");
+const conPKeys = Object.keys(siteContent["contact"]).filter(x => !x.includes("h4"));
+conP.forEach(function(element, index){
+  element.textContent = siteContent.contact[conPKeys[index]];
+});
+
+//finish it off with the footer
+const copy = document.querySelector("footer p");
+copy.textContent = siteContent.footer.copyright;
